89c545c20b9cbd818a567f735d44dc6a08781bf6
Andy Beck <beckandy@umich.edu>
2018-08-06 15:35:58 -0400

    scripts for extracting motif counts

---
+++ data_mgmt/data_prep/count_motifs_batch.pl
@@ -0,0 +1,52 @@
+#!/usr/local/bin/perl
+
+##############################################################################
+# Create & execute batch file to get extended summary files
+##############################################################################
+use strict;
+use warnings;
+use POSIX;
+use File::Basename;
+use File::Path qw(make_path);
+use FindBin;
+use YAML::XS 'LoadFile';
+use feature 'say';
+
+my $relpath = $FindBin::Bin;
+my $configpath = dirname(dirname($relpath));
+my $config = LoadFile("$configpath/_config.yaml");
+
+my $email = $config->{email};
+my $analysisdir = $config->{analysisdir};
+
+use lib "$FindBin::Bin/../lib";
+use SmaugFunctions qw(forkExecWait getRef);
+
+my $today = POSIX::strftime('%Y%m%d', localtime);
+my $slurmdir = "$analysisdir/output/slurm/$today";
+  make_path("$slurmdir");
+
+for my $adj (1 .. 4) {
+  # my $nbp=$adj*2+1;
+
+  my $jobcmd="augment_summaries_$adj";
+  my $builddatbatch = "$analysisdir/slurm/$jobcmd.txt";
+  open my $mdFH, '>', $builddatbatch or die "can't write to $builddatbatch: $!\n";
+  print $mdFH "#!/bin/sh \n";
+  print $mdFH "#SBATCH --mail-type=FAIL \n";
+  print $mdFH "#SBATCH --mail-user=$email \n";
+  print $mdFH "#SBATCH --ntasks=1 \n";
+  print $mdFH "#SBATCH --mem=4000 \n";
+  print $mdFH "#SBATCH --time 00:30:00 \n";
+  print $mdFH "#SBATCH --job-name=$jobcmd \n";
+  print $mdFH "#SBATCH --partition=nomosix \n";
+  print $mdFH "#SBATCH --array=1-22 \n";
+  print $mdFH "#SBATCH --requeue \n";
+  print $mdFH "#SBATCH --output=\"$slurmdir/slurmJob-%J.out\" --error=\"$slurmdir/slurmJob-%J.err\" \n";
+  print $mdFH "srun perl $relpath/count_motifs.pl \${SLURM_ARRAY_TASK_ID} $adj";
+  close($mdFH) or die "Unable to close file: $builddatbatch $!";
+
+  my $slurmcmd="sbatch $builddatbatch";
+  forkExecWait($slurmcmd);
+
+}
