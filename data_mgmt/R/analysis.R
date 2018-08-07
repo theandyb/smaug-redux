fafee4d768b876439832894dcd88736b480f73e6
Andy Beck <beckandy@umich.edu>
2018-08-07 08:03:40 -0400

    NMF code

--- data_mgmt/R/analysis.R
+++ data_mgmt/R/analysis.R
@@ -48,12 +48,158 @@ cbp <- adj + 1 # I'm guessing cbp = "center base pair"?
 
 bins1Mb <- get_bins(paste0(analysisdir, "/motif_counts/3-mers/full"), "1000kb_full.txt")
 bins1Mb$CHR <- gsub("chr", "", bins1Mb$CHR)
+# RESULT: CHR, Start, End, Bin, Motif, nMotifs -> how many of motif do we see in each 1000kb window?
+
+sub_mer <- function(motif, n){
+  lMotif <- (nchar(motif) - 2) / 2
+  center <- ceiling(lMotif / 2)
+  SEQA <- substr(motif, center - n, center + n)
+  SEQB <- substr(motif, (center * 3) - n, (center * 3) + n)
+  return(paste0(SEQA, "(", SEQB, ")"))
+}
+
+get_mct_b <- function(bins){
+  out <- bins %>%
+  	group_by(CHR, BIN, Motif) %>%
+  	summarise(nMotifs=sum(nMotifs))
+  return(out)
+}
 
 # Count singletons per 3-mer subtype per individual
 ind_counts <- full_data$sites %>%
   mutate(Type=gsub("cpg_", "", Category2),
-    SEQA=substr(Motif, cbp-i, cbp+i),
-    SEQB=substr(Motif, cbp*3-i, cbp*3+i),
-    Motif=paste0(SEQA, "(", SEQB, ")")) %>%
+    Motif=sub_mer(Motif,1)) %>%
   group_by(ID, Type, Motif) %>%
   summarise(n=n())
+# RESULT: ID, Type, Motif, n
+
+# count 3-mer motifs genome-wide
+mct3 <- get_mct_b(bins1Mb) %>%
+  group_by(Motif) %>%
+  summarise(nMotifs=sum(nMotifs))
+# RESULT : Motif, nMotif
+
+# get ids in ped file
+ped <- read.table(pedfile, header=F, stringsAsFactors=F)
+names(ped)[1] <- "ID"
+
+pheno <- read.table(phenofile, header=T, stringsAsFactors=F)
+pheno <- pheno %>%
+  dplyr::select(ID=SEQID, Sex, Case.Control, Study, DROP) %>%
+  filter(!(is.na(ID)))
+# RESULT : ID, Sex, Case.Control, Study, DROP
+
+qplotdat <- read.table(qplotfile, header=T, stringsAsFactors=F)
+names(qplotdat)[1] <- "ID"
+
+contam <- read.table(contamfile, header=T, stringsAsFactors=F)
+names(contam) <- c("ID", "FREEMIX", "CHIPMIX", "PLATE")
+
+vcfast <- read.table(vcfastfile,
+  header=T, stringsAsFactors=F, sep="\t", comment.char=" ")
+names(vcfast) <- c("ID", "SNPs", "Singletons", "Doubletons", "lt0.5", "gt0.5", "Ref", "Het", "Alt", "Heterozygosity")
+
+pcs <- read.table(pcfile, header=F, stringsAsFactors=F, skip=1)
+names(pcs) <- c("ID", paste0("PC",1:10), "Study")
+pcs <- pcs %>%
+  dplyr::select(-Study)
+
+subjectData <- merge(pheno, pcs, by="ID") %>%
+  merge(vcfast, by="ID") %>%
+  merge(contam, by="ID", all.x=TRUE) %>% distinct(ID, .keep_all = TRUE)
+
+ped_b_ids <- subjectData[!(subjectData$ID %in% qplotdat$ID),] %>%
+  dplyr::select(ID_B=ID) %>%
+  mutate(ID=gsub("B", "", ID_B))
+
+# Subset ped2 to B ids
+qplotB <- qplotdat %>%
+  filter(ID %in% ped_b_ids$ID) %>%
+  mutate(ID=paste0(ID, "B"))
+
+qplot2 <- rbind(qplotdat, qplotB)
+subjectData <- merge(subjectData, qplot2, by="ID")
+
+# get per-person rates and filter IDs
+ind_counts <- merge(ind_counts, mct3, by="Motif") %>%
+  filter(ID %in% subjectData$ID) %>%
+  mutate(ERV_rel_rate=n/nMotifs,
+    subtype=paste0(Type, "_", Motif))
+
+###############################################################################
+# get signature loadings
+###############################################################################
+get_loads <- function(widedat, nmfdat){
+  sigloads <- data.frame(subtype=names(widedat),
+    sig1=coef(nmfdat)[1,],
+    sig2=coef(nmfdat)[2,],
+    sig3=coef(nmfdat)[3,]) %>%
+    mutate(sig1=sig1/sum(sig1), sig2=sig2/sum(sig2), sig3=sig3/sum(sig3)) %>%
+    gather(subtype, value)
+
+  names(sigloads) <- c("subtype", "sig", "value")
+  sigloads <- sigloads %>%
+    mutate(Category=substr(subtype, 1, 5),
+      Sequence=substr(subtype, 7, 14))
+
+  return(sigloads)
+}
+
+###############################################################################
+# plot signature loadings
+###############################################################################
+plot_loads <- function(sigloads){
+  p <- ggplot(sigloads, aes(x=Sequence, y=value, fill=sig))+
+    geom_bar(stat="identity")+
+    facet_grid(sig~Category, scales="free_x")+
+    # geom_label_repel(data=sigloads[sigloads$sig3>0.005,], aes(x=sig2, y=sig3, label=subtype))+
+    theme_bw()+
+    theme(axis.text.x=element_text(angle=90, hjust=1),
+      strip.text=element_text(size=16),
+      legend.position="none")
+  return(p)
+}
+
+###############################################################################
+# plot signature contribution across individuals
+###############################################################################
+plot_ind_sigs <- function(sigdat){
+  p <- ggplot(sigdat,
+      aes(x=ID, y=prob, group=factor(Signature), colour=factor(Signature)))+
+    geom_line()+
+    scale_x_discrete(expand=c(0,0))+
+    scale_y_continuous(expand=c(0,0), limits=c(0,1))+
+    facet_wrap(~Study, scales="free_x", nrow=1)+
+    ylab("signature contribution")+
+    theme_bw()+
+    theme(axis.text.x=element_blank(),
+      legend.position="bottom")
+
+  return(p)
+}
+
+plot_ind_sigs2 <- function(sigdat){
+  p <- ggplot(sigdat,
+      aes(x=ID, y=prob, group=factor(Signature), colour=factor(Signature)))+
+    geom_line()+
+    scale_x_discrete(expand=c(0,0))+
+    scale_y_continuous(expand=c(0,0), limits=c(0,1))+
+    facet_wrap(~top_r, scales="free_x", nrow=1)+
+    ylab("signature contribution")+
+    theme_bw()+
+    theme(axis.text.x=element_blank(),
+      legend.position="bottom")
+
+  return(p)
+}
+
+###############################################################################
+# Prepare data and run NMF
+###############################################################################
+ind_wide <- ind_counts  %>%
+  dplyr::select(ID, subtype, ERV_rel_rate) %>%
+  spread(subtype, ERV_rel_rate)
+ind_wide[is.na(ind_wide)] <- 0
+# RESULT: subject x rel_rates_per_3mer
+
+ind_nmf <- nmf(as.matrix(ind_wide[,-c(1)]), 3, nrun=10)
