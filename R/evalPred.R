89fb16c8d014f49746c8262cfa4c10082a5996ec
Andy <beckandy@umich.edu>
2018-09-09 17:51:56 -0400

    bug fix

--- R/evalPred.R
+++ R/evalPred.R
@@ -19,17 +19,9 @@ get_sum <- function(pred.name){
   # What do we want to get? Min, Max, Var, Number > 0.5
   # load the data
   dat <- read_tsv(pred.name, col_names = FALSE)
-  chr <- pred.name %>% str_extract("chr[0-9]+") %>%
-            str_extract("[0-9]+") %>% as.numeric
+  chr <- pred.name %>% str_extract("chr[0-9]+") %>% str_extract("[0-9]+") %>% as.numeric
   type <- str_extract(pred.name, "[A,C,G,T]{2}_[A,C,G,T]{2}")
-  return tibble(
-            chr = chr,
-            type = type,
-            min = min(dat$X3),
-            max = max(dat$X3),
-            var = var(dat$X3),
-            highly = sum(dat$X3 > 0.5)
-  )
+  return tibble(chr = chr, type = type, min = min(dat$X3), max = max(dat$X3), var = var(dat$X3), highly = sum(dat$X3 > 0.5))
 }
 
 res <- mclapply(pred.files, get_sum, mc.cores = detectCores())
