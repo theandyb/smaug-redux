f02f3a0e0c544df26ee60a4f9a946a3fd74a8114
Beck <beckandy@UMICH.EDU>
2018-09-09 14:55:09 -0400

    Change parentdir to analysisdir

--- data_mgmt/process_predicted/sort_pred_batch.pl
+++ data_mgmt/process_predicted/sort_pred_batch.pl
@@ -19,20 +19,20 @@ my $config = LoadFile("$configpath/_config.yaml");
 my $adj = 3;
 my $data = $config->{data};
 my $email = $config->{email};
-my $parentdir = $config->{parentdir};
+my $analysisdir = $config->{analysisdir};
 
 use lib "$FindBin::Bin/../lib";
 use SmaugFunctions qw(forkExecWait getRef);
 
 my $today = POSIX::strftime('%Y%m%d', localtime);
-my $slurmdir = "$parentdir/output/slurm/$today";
+my $slurmdir = "$analysisdir/output/slurm/$today";
   make_path("$slurmdir");
 
 my @categs = qw( AT_CG AT_GC AT_TA GC_AT GC_CG GC_TA );
 
 # foreach my $categ (@categs){
   # my $jobcmd="${categ}_sort_pred";
-  my $builddatbatch = "$parentdir/slurm/sort_pred_batch.txt";
+  my $builddatbatch = "$analysisdir/slurm/sort_pred_batch.txt";
   open my $mdFH, '>', $builddatbatch or die "can't write to $builddatbatch: $!\n";
   print $mdFH "#!/bin/bash \n";
   print $mdFH "#SGRIDBATCH CAT='AT_CG' 'AT_GC' 'AT_TA' 'GC_AT' 'GC_CG' 'GC_TA'\n";
@@ -50,7 +50,7 @@ my @categs = qw( AT_CG AT_GC AT_TA GC_AT GC_CG GC_TA );
   print $mdFH "export STDERR=\"$slurmdir/chr\$INDEX.\$CAT.err\"\n";
   # print $mdFH "#SBATCH --exclude=psoriasis-mc01,psoriasis-mc02 \n";
   # print $mdFH "#SBATCH --output=\"$slurmdir/slurmJob-%J.out\" --error=\"$slurmdir/slurmJob-%J.err\" \n";
-  print $mdFH "srun cat $parentdir/output/predicted/\$CAT/chr\$INDEX/* | sort -V -k2 > $parentdir/output/predicted/chr\$INDEX.\$CAT.txt";
+  print $mdFH "srun cat $analysisdir/output/predicted/\$CAT/chr\$INDEX/* | sort -V -k2 > $analysisdir/output/predicted/chr\$INDEX.\$CAT.txt";
   close($mdFH) or die "Unable to close file: $builddatbatch $!";
 
   my $slurmcmd="gridbatch $builddatbatch";
