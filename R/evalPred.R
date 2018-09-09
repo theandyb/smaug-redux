90a780304d6405b7ad30a067b7d7f6efc6f11820
Andy Beck <beckandy@umich.edu>
2018-09-09 16:38:45 -0400

    Dealing with directories

--- R/evalPred.R
+++ R/evalPred.R
@@ -3,4 +3,5 @@ initial.options <- commandArgs(trailingOnly = FALSE)
 file.arg.name <- "--file="
 script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
 script.basename <- dirname(script.name)
-print(script.basename)
+setwd(script.basename)
+print(getwd())
