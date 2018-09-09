a778b94929ac68b923cefba71a3c620883e77db8
Beck <beckandy@UMICH.EDU>
2018-09-09 16:33:25 -0400

    output basename

--- R/evalPred.R
+++ R/evalPred.R
@@ -3,3 +3,4 @@ initial.options <- commandArgs(trailingOnly = FALSE)
 file.arg.name <- "--file="
 script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
 script.basename <- dirname(script.name)
+print(script.basename)
