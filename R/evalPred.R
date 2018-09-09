207f50ebc2780f815750e6078517db4c0ed91d14
Beck <beckandy@UMICH.EDU>
2018-09-09 16:46:05 -0400

    Testing up to parsing files

--- R/evalPred.R
+++ R/evalPred.R
@@ -4,5 +4,12 @@ initial.options <- commandArgs(trailingOnly = FALSE)
 script.basename <- sub("--file=", "", initial.options[grep("--file=", initial.options)]) %>%
   dirname
 setwd(script.basename)
+
 args <- yaml.load_file("../_config.yaml")
-print(args)
+attach(args)
+
+pred.files.dir <- paste0(analysisdir, "/output/predicted")
+setwd(pred.files.dir)
+
+pred.files <- Sys.glob("chr*.txt")
+print(pred.files)
