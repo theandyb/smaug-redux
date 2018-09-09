c07c7c7e852aae459d4e6a534c1d2da1a8193201
Andy <beckandy@umich.edu>
2018-09-09 17:46:55 -0400

    parallelize the job

--- R/evalPred.R
+++ R/evalPred.R
@@ -1,6 +1,7 @@
 library("tidyverse")
 library("stringr")
 library("yaml")
+library("parallel")
 initial.options <- commandArgs(trailingOnly = FALSE)
 script.basename <- sub("--file=", "", initial.options[grep("--file=", initial.options)]) %>%
   dirname
@@ -12,11 +13,9 @@ attach(args)
 pred.files.dir <- paste0(analysisdir, "/output/predicted")
 setwd(pred.files.dir)
 
-# Empty tibble to store results
-res <- NULL
-
 pred.files <- Sys.glob("chr*.txt")
-for(pred.name in pred.files){
+#for(pred.name in pred.files){
+get_sum <- function(pred.name){
   # What do we want to get? Min, Max, Var, Number > 0.5
   # load the data
   dat <- read_tsv(pred.name, col_names = FALSE)
@@ -24,14 +23,18 @@ for(pred.name in pred.files){
             str_extract("[0-9]+") %>% as.numeric
   type <- pred.name %>%
             str_extract("[A,C,G,T]{2}_[A,C,G,T]{2}")
-  res <- bind_rows(res, tibble(
+  return tibble(
             chr = chr,
             type = type,
             min = min(dat$X3),
             max = max(dat$X3),
             var = var(dat$X3),
             highly = sum(dat$X3 > 0.5)
-  ))
+  )
 }
 
-write.csv("explore.csv")
+res <- mclapply(pred.files, get_sum, mc.cores = detectCores())
+
+res.table <- bind_rows(res)
+
+write.csv(res.table, file = "explore.csv")
