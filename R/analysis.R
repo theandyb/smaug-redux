43652680f33870d01dba9a0181a2190e2e2d5c6f
Andy Beck <beckandy@umich.edu>
2018-10-10 16:07:16 -0400

    Indentation for easier reading

--- R/analysis.R
+++ R/analysis.R
@@ -498,7 +498,7 @@ sig_anc <- test_anc %>%
 	arrange(desc(prop))
 
 
-	if(build_logit){
+if(build_logit){
 	cat("Preparing data for logistic regression model...\n")
 
 	i<-3
@@ -506,17 +506,17 @@ sig_anc <- test_anc %>%
 		posfile <- paste0(analysisdir,
 		"/output/logmod_data/chr", chr, "_sites.txt")
 		dat <- full_data$sites %>%
-		# summfile1 <- sites %>%
-		# filter(Category==categ) %>%
-		filter(CHR==chr) %>%
-		mutate(Type=gsub("cpg_", "", Category2),
-		SEQA=substr(Motif, cbp-i, cbp+i),
-		SEQB=substr(Motif, cbp*3-i, cbp*3+i),
-		Sequence=paste0(SEQA, "(", SEQB, ")")) %>%
-		dplyr::select(CHR, POS, Sequence, Type) %>%
-		mutate(mut=1) %>%
-		spread(Type, mut, fill=0)
+			# summfile1 <- sites %>%
+			# filter(Category==categ) %>%
+			filter(CHR==chr) %>%
+			mutate(Type=gsub("cpg_", "", Category2),
+				SEQA=substr(Motif, cbp-i, cbp+i),
+				SEQB=substr(Motif, cbp*3-i, cbp*3+i),
+				Sequence=paste0(SEQA, "(", SEQB, ")")) %>%
+			dplyr::select(CHR, POS, Sequence, Type) %>%
+			mutate(mut=1) %>%
+			spread(Type, mut, fill=0)
 
 		write.table(dat, posfile, col.names=F, row.names=F, quote=F, sep="\t")
-		}
 	}
+}
