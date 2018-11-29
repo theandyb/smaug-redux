1e73234587b28eec5104b444f48a6143f25c67f7
Andy Beck <beckandy@umich.edu>
2018-11-29 09:47:32 -0500

    Script for adding motif counts for reverse complements

---
+++ data_mgmt/lib/rc_motif_count.r
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(stringi)
+chromosomes <- seq(1,22)
+results_dir <- "/net/snowwhite/home/beckandy/research/smaug-redux/motif_counts/5-mers/full"
+
+for(chr in chromosomes){
+    cntFile <- paste0(results_dir, "/", "chr", chr, "_bins.tsv")
+    df <- read_tsv(cntFile, col_names = c('Motif', 'nMotif', 'BIN'))
+    df <- df %>% mutate(RC = stri_reverse(chartr("ACGT","TGCA", Motif)))
+    df <- inner_join(select(df, Motif, RC, BIN, nMotif), 
+        select(df, RC, BIN, nMotif),
+        by = c("Motif" = "RC", "BIN" = "BIN"))
+    df['nMotifs'] <- df$nMotif.x + df$nMotif.y
+    df <- df %>% mutate(Motif = paste0(Motif, "(", RC ,")")) %>%
+        select(Motif, BIN, nMotifs)
+    outFile <- paste0(results_dir, "/", "chr", chr, ".5-mer_motifs_10000kb_full.txt")
+    write.table(df, outFile, quote = FALSE, row.name = FALSE)
+}
