a561012f124be17807eb5d73faa46fe22ab4d0da
Andy Beck <beckandy@umich.edu>
2018-12-07 15:13:17 -0500

    Edit col name

--- data_mgmt/lib/rc_motif_count.r
+++ data_mgmt/lib/rc_motif_count.r
@@ -5,12 +5,12 @@ results_dir <- "/net/snowwhite/home/beckandy/research/smaug-redux/motif_counts/3
 
 for(chr in chromosomes){
     cntFile <- paste0(results_dir, "/", "chr", chr, "_bins.tsv")
-    df <- read_tsv(cntFile, col_names = c('Motif', 'nMotif', 'BIN'))
+    df <- read_tsv(cntFile, col_names = c('Motif', 'nMotifs', 'BIN'))
     df <- df %>% mutate(RC = stri_reverse(chartr("ACGT","TGCA", Motif)))
-    df <- inner_join(select(df, Motif, RC, BIN, nMotif), 
-        select(df, RC, BIN, nMotif),
+    df <- inner_join(select(df, Motif, RC, BIN, nMotifs), 
+        select(df, RC, BIN, nMotifs),
         by = c("Motif" = "RC", "BIN" = "BIN"))
-    df['nMotifs'] <- df$nMotif.x + df$nMotif.y
+    df['nMotifs'] <- df$nMotifs.x + df$nMotifs.y
     df <- df %>% mutate(Motif = paste0(Motif, "(", RC ,")")) %>%
         select(Motif, BIN, nMotifs)
     outFile <- paste0(results_dir, "/", "chr", chr, ".3-mer_motifs_1000kb_full.txt")
